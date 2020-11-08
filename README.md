5dba01fc131dd8fc6522d3d12d9f630aa1a3acc1
Oyvind Hansen <oyvind.hansen@outlook.com>
2020-11-08 13:27:44 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # bme680-signalk-ws
 Send bme680 data to SignalK server from edge device
+
+## Use
+- Requires a bme680-sensor
+- Setup a new user in SignalK
+- Provide user name and password to the script, where [USER] and [PWD] is defined
+- Edit the URL IP address to the SignalK server where [IP] is defined
+- Run the script as normal `node index.js`, or with logging output: `node index.js log`
+
+**Disclaimer:** This code DOES WORK, but is still VERY BETA
+
+**Points to improve**
+- Discovery of SignalK server
+- Reconnect on disconnect or restart of server
+
+## Contribute
+Feel free to contribute by creating a *Pull Request*
---
+++ index.js
@@ -0,0 +1,114 @@
+#!/usr/bin/env node
+
+var uuid = require('uuid');
+const WebSocket = require('ws');
+const { Bme680 } = require('bme680-sensor');
+
+const bme680 = new Bme680(1, 0x76);
+const ws = new WebSocket('ws://192.168.43.9:3000/signalk/v1/stream?subscribe=none')
+
+var loggingEnabled = process.argv[2] == "log";
+
+console.info('server started');
+
+ws.on('open', function open() {
+    ws.send(JSON.stringify({
+        requestId: uuid.v4(),
+        login: {
+            username: '[USER]', //signalk user
+            password: '[PWD]' //password
+        }
+    }));
+    log('login sent');
+});
+
+var hasToken = false;
+var token = '';
+
+
+ws.on('message', function incoming(data) {
+    var result = JSON.parse(data)
+
+    if (!hasToken) {
+        var pos = data.search('token');
+        if (pos != -1) {
+            token = result.login.token;
+            hasToken = true;
+            readSensor();
+            log('token received')
+        }
+    }
+});
+
+ws.on('close', function close() {
+    log('disconnected');
+});
+
+
+function readSensor() {
+    log('start initialize sensor');
+    bme680.initialize().then(async () => {
+        console.info('sensor initialized')
+        setInterval(async () => {
+            var payload = signalkMessage(await bme680.getSensorData());
+            ws.send(payload);
+            log("payload sent");
+        }, 1000);
+    });
+}
+
+function signalkMessage(sensorJSON) {
+    var payload = JSON.stringify({
+        context: 'vessels.urn:mrn:imo:mmsi:219019288',
+        updates: [{
+            source: {
+                type: 'bme680',
+                src: 'rpisensors',
+                pgn: 123,
+                label: 'bme680-sensor'
+            },
+            timestamp: new Date().toISOString(),
+            values: [
+                {
+                    path: 'environment.outside.temperature',
+                    value: toKelvin(sensorJSON.data.temperature)
+                },
+                {
+                    path: 'environment.outside.pressure',
+                    value: toPascal(sensorJSON.data.pressure)
+                },
+                {
+                    path: 'environment.outside.humidity',
+                    value: sensorJSON.data.humidity
+                },
+                {
+                    path: 'environment.outside.gas_resistance',
+                    value: round(sensorJSON.data.gas_resistance, 1)
+                }]
+        }]
+    });
+
+    return payload;
+}
+
+function toKelvin(celcius) {
+    return round(celcius + 273.15, 1);
+}
+
+function toPascal(hPa) {
+    return hPa * 100;
+}
+
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
+function log(message) {
+    if (loggingEnabled) {
+        console.log(message);
+    }
+}
+
+
+
